df2c9d9b304b6fe67847542b06ec3016ea3572d0
devloop <devloop307@gmail.com>
2026-03-08 18:37:08 -0500

    exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a,  b) {
+    let acumulador = 0;
+    if (a < 0 || b < 0) return "ERROR";
+    if(Number.isInteger(a) && Number.isInteger(b)){
+        if(a < b){
+            for(let i = a; i <= b; i++){
+                acumulador += i;
+            }
+        }else{
+            for(let i = b; i <= a; i++){
+                acumulador += i;
+            }
+        }
+    }else{
+        return "ERROR";
+    }
+    return acumulador;
 };
-
 // Do not edit below this line
 module.exports = sumAll;

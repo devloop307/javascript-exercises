663b07340f4b55e362a634ec4ee2471065af3503
devloop <devloop307@gmail.com>
2026-03-08 12:56:56 -0500

    exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
+}
 
+/*
+const reverseString = function(str) {
+    let result = ""
+    for(let i = str.length-1; i > -1; i --){
+        result += str[i];
+    }
+    return result;
 };
+*/
 
 // Do not edit below this line
 module.exports = reverseString;

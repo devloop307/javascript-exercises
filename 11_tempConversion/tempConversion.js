8428194c21472762d4c5d75abc0d8c7c84940fb5
devloop <devloop307@gmail.com>
2026-03-10 22:58:08 -0500

    exercise 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numFa) {
+  let celcius = ((numFa - 32) / 1.8);
+  return Math.round(celcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(numCe) {
+  let Fahrenheit = (1.8 * numCe) + 32;
+  return Math.round(Fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
